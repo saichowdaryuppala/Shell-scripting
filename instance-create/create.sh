6eaa65677df314bbb198e07928179ffc0903131c
saichowdaryuppala <saichowdaryuppala@gmail.com>
2021-08-14 17:10:57 -0400

    launch instance.

--- instance-create/create.sh
+++ instance-create/create.sh
@@ -2,7 +2,15 @@
 
 LID=lt-038a9467b415f57a7
 LVER=2
-aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} |jq
+COMPONENT=$1
+if  [ -z "${COMPONENT}" ]; then
+  echo "Component Name input is needed"
+  exit 1
+fi
+
+aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}   --tag-specifications "ResourceType=instance,
+Tags=[{Key=Name,Value=${COMPONENT}]" |jq
+
 
 
 
