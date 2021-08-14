fcb53ace075cc7ab61ca9ec53b03aa89f6cb5334
saichowdaryuppala <saichowdaryuppala@gmail.com>
2021-08-14 17:26:41 -0400

    Create a tag.

---
+++ instance-create/tag.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+LID=lt-038a9467b415f57a7
+LVER=2
+COMPONENT=$1
+if  [ -z "${COMPONENT}" ]; then
+  echo "Component Name input is needed"
+  exit 1
+fi
+
+aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}   --tag-specifications "ResourceType=instance,
+Tags=[{Key=Name,Value=${COMPONENT}}]" |jq
